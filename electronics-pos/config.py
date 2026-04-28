8607724e5ad3df9dc2d1ec4e0a314c926567b4fb
Trishia Kyle Cuyos <uclm.trishia@gmail.com>
2026-04-28 09:39:06 +0800

    update project

--- electronics-pos/config.py
+++ electronics-pos/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key-change-this'
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///pos_system.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
